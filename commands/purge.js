e8b67bec571295ddd30c108cbd6b879409de751c
BuildTools <unconfigured@null.spigotmc.org>
2020-12-28 21:11:35 -0700

    just changed something in the purge command

--- commands/purge.js
+++ commands/purge.js
@@ -10,15 +10,17 @@ module.exports = {
   permissions: ['MANAGE_MESSAGES'],
   category: 'moderation',
   execute(message, args) {
-    const deleteMessages = parseInt(args[0]) + 1;
+    const amount = parseInt(args[0]) + 1;
     const embed = new Discord.MessageEmbed()
       .setColor('#0caecf')
       .setTitle('Invalid Value')
       .setDescription('You must choose an integer that is between 1 and 99')
 
-    if (0 > args[0] || args[0] > 99) {
+    if (amount <= 1 || amount > 100) {
       return message.channel.send(embed);
     }
-      return message.channel.bulkDelete(deleteMessages);
+      return message.channel.bulkDelete(amount, true).catch(err => {
+				console.error(err);
+			})
   },
 }
