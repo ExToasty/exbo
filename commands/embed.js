883390983d7e3b2179611295fecb319c0df521d7
BuildTools <unconfigured@null.spigotmc.org>
2020-12-29 12:26:04 -0700

    Added some more configurations

--- commands/embed.js
+++ commands/embed.js
@@ -1,31 +1,36 @@
-"use strict";
-
+'use strict';
 const Discord = require('discord.js');
 const { prefix, embedColor } = require('../config.json');
 
 
 module.exports = {
-  name: 'embed',
-  description: 'Embeds the agruments specified.',
-  args: true,
-  usage: `${prefix}embed <message>`,
-  category: 'chat',
-  deleteMessage: true,
-  execute(message, args) {
-    let embed = new Discord.MessageEmbed()
-    if (message.author.id !== '332555969169063938' && args.includes('@everyone') || args.includes('@here') && !message.member.hasPermission('ADMINISTRATOR')) {
-      embed
-        .setColor(embedColor)
-        .setTitle('Insufficient Permissions')
-        .setDescription(`You don't have the permissions to say this in your message.`)
-      
-      return message.channel.send(embed);
-    }
+	name: 'embed',
+	description: 'Embeds the agruments specified.',
+	usage: `${prefix}embed <message>`,
+	guildOnly: false,
+	args: true,
+	selfExecute: false,
+	requireMention: false,
+	wip: false,
+	cooldown: 3,
+	deleteMessage: true,
+	permissions: ['SEND_MESSAGES'],
+	category: 'chat',
+	execute(message, args) {
+		const embed = new Discord.MessageEmbed();
+		if (message.author.id !== '332555969169063938' && args.includes('@everyone') || args.includes('@here') && !message.member.hasPermission('ADMINISTRATOR')) {
+			embed
+				.setColor(embedColor)
+				.setTitle('Insufficient Permissions')
+				.setDescription(`You don't have the permissions to say this in your message.`);
 
-    embed
-      .setColor(embedColor)
-      .setDescription(args.join(' '))
+			return message.channel.send(embed);
+		}
 
-    message.channel.send(embed);
-  }
-}
+		embed
+			.setColor(embedColor)
+			.setDescription(args.join(' '));
+
+		message.channel.send(embed);
+	},
+};
