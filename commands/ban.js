bbe02fd4b99f60cb9bb8e2b4c6437f71b189d1b4
BuildTools <unconfigured@null.spigotmc.org>
2020-12-29 12:27:05 -0700

    Added some more configurations to the export

--- commands/ban.js
+++ commands/ban.js
@@ -1,26 +1,31 @@
-"use strict"
+'use strict';
 
 const Discord = require('discord.js');
 const { prefix, embedColor } = require('../config.json');
 
 module.exports = {
-  name: 'ban',
-  description: 'Bans the user specified from the current server',
-  guildOnly: true,
-  args: true,
-  usage: `${prefix}ban <user> [reason]`,
-  permissions: 'ADMINISTRATOR',
-  category: 'moderation',
-  selfExecute: false,
-  async execute(message, args) {
-    let user = message.mentions.users.first();
-    const reason = args.slice(0).join(' ');
-    const embed = new Discord.MessageEmbed()
-      .setColor(embedColor)
-      .setTitle('Ban Succesful')
-      .setDescription(`:white_check_mark: ${user} has been banned`)
+	name: 'ban',
+	description: 'Bans the user specified from the current server',
+	usage: `${prefix}ban <user> [reason]`,
+	// aliases: [''],
+	guildOnly: true,
+	args: true,
+	selfExecute: false,
+	requireMention: true,
+	wip: false,
+	cooldown: 3,
+	deleteMessage: true,
+	permissions: 'ADMINISTRATOR',
+	category: 'moderation',
+	async execute(message, args) {
+		const user = message.mentions.users.first();
+		const reason = args.slice(0).join(' ');
+		const embed = new Discord.MessageEmbed()
+			.setColor(embedColor)
+			.setTitle('Ban Succesful')
+			.setDescription(`:white_check_mark: ${user} has been banned`);
 
-    await message.guild.members.ban(user, { reason: reason });
-    message.channel.send(embed);
-  }
-}
+		await message.guild.members.ban(user, { reason: reason });
+		message.channel.send(embed);
+	},
+};
