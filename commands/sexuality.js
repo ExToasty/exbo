3b65d5665f92a62e3bcc72f78d34808bddf7858b
BuildTools <unconfigured@null.spigotmc.org>
2020-12-30 00:38:45 -0700

    Fixing running the command without pinging user.

--- commands/sexuality.js
+++ commands/sexuality.js
@@ -11,7 +11,6 @@ module.exports = {
 	permissions: 'SEND_MESSAGES',
 	usage: `${prefix}sexuality [mention]`,
 	cooldown: 5,
-	requireMention: true,
 	category: 'fun',
 	execute(message, args) {
 		const percentage = Math.floor(Math.random() * 100) + 1;
@@ -19,15 +18,20 @@ module.exports = {
 		const sexuality = responses[Math.floor(Math.random() * responses.length)];
 		const embed = new Discord.MessageEmbed()
 			.setColor(embedColor)
-			.setTitle('Gay Calculator')
-			.setDescription(`You are ${percentage}% ${sexuality}`);
+			.setTitle('__Sexuality Test__');
 
 		if (!args.length) {
+			embed
+				.setDescription(`You are ${percentage}% ${sexuality}`);
 			return message.channel.send(embed);
 		}
-		else {
-			embed.setDescription(`${args[0]} is ${percentage}% ${sexuality}`);
-			message.channel.send(embed);
+		else if (message.mentions.users.first()) {
+			embed
+				.setTitle('__Invalid Arguement__')
+				.setDescription('You need to mention a user.');
+			return message.channel.send(embed);
 		}
+		embed.setDescription(`${args[0]} is ${percentage}% ${sexuality}`);
+		message.channel.send(embed);
 	},
 };
