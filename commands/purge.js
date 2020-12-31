33c9e661082a776261b9605238e1d7de9014d512
ExToasty <ExToasty>
2020-12-31 13:22:26 -0700

    Added some more information to the export module.

--- commands/purge.js
+++ commands/purge.js
@@ -4,19 +4,25 @@ const { prefix, embedColor } = require('../config.json');
 module.exports = {
 	name: 'purge',
 	description: 'Deletes the specified amount of messages.',
-	aliases: ['clear', 'delete'],
 	usage: `${prefix}purge <positive integer>`,
+	aliases: ['clear', 'delete'],
+	guildOnly: true,
 	args: true,
+	selfExecute: false,
+	requireMention: false,
+	wip: false,
+	cooldown: 3,
+	deleteMessage: true,
 	permissions: ['MANAGE_MESSAGES'],
 	category: 'moderation',
 	execute(message, args) {
-		const amount = parseInt(args[0]) + 1;
+		const amount = parseInt(args[0]);
 		const embed = new Discord.MessageEmbed()
 			.setColor(embedColor)
 			.setTitle('Invalid Value')
 			.setDescription('You must choose an integer that is between 1 and 99');
 
-		if (amount <= 1 || amount > 100) {
+		if (amount < 1 || amount > 100) {
 			return message.channel.send(embed);
 		}
 		return message.channel.bulkDelete(amount, true).catch(err => {
