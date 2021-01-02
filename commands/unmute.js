fcad9f41920a138abcf814e9d792cc9284b2d507
ExToasty <ExToasty>
2021-01-01 23:24:41 -0700

    Changing some more variables and stuff.

--- commands/unmute.js
+++ commands/unmute.js
@@ -3,7 +3,7 @@ const { prefix, embedColor } = require('../config.json');
 
 module.exports = {
 	name: 'unmute',
-	description: 'If the specified user is muted, the it unmutes them.',
+	description: 'If the specified member is muted, the it unmutes them.',
 	usage: `\`${prefix}unmute <user> [reason]\``,
 	permissions: ['MANAGE_ROLES'],
 	guildOnly: true,
@@ -13,32 +13,33 @@ module.exports = {
 	deleteMessage: true,
 	category: 'moderation',
 	wip: true,
-	execute(message, args, getUserFromMention) {
+	execute(message, args) {
 		const reason = args.slice(1).join(' ');
-		const mutedRole = message.user.roles.cache.some(role => role.name === 'muted');
-		const user = getUserFromMention(args[0]);
+		const mutedRole = message.member.roles.cache.some(role => role.name === 'muted');
+		const target = message.mentions.members.first();
+		const member = message.guild.members.cache.get(target.id);
 		const embed = new Discord.MessageEmbed().setColor(embedColor);
-		if (!user.roles.cache.some(role => role.name === 'muted')) {
+		if (!member.roles.cache.some(role => role.name === 'muted')) {
 			embed
 				.setTitle('__Unmute Unsuccesful__')
-				.setDescription(`__**\`${user}\`**__\` is not muted\``);
+				.setDescription(`__**\`${target}\`**__\` is not muted\``);
 
 			return message.channel.send(embed);
 		}
 		if (!reason) {
 			embed
 				.setTitle('__Unmute Succesful__')
-				.setDescription(`__**\`${user}\`**__\` has been unmuted.\``);
+				.setDescription(`__**\`${target}\`**__\` has been unmuted.\``);
 
-			return user.roles.remove(mutedRole)
+			return member.roles.remove(mutedRole)
 				.then(message.channel.send(embed))
 				.catch();
 		}
 		embed
 			.setTitle('Unmute Succesful')
-			.setDescription(`__**\`${user}\`**__\` has been unmuted for ${reason}\``);
+			.setDescription(`__**\`${target}\`**__\` has been unmuted for ${reason}\``);
 
-		return user.roles.remove(mutedRole, [reason])
+		return member.roles.remove(mutedRole, [reason])
 			.then(message.channel.send(embed))
 			.catch();
 
