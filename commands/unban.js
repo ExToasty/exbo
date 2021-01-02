34e586f7cc163bb2023658f6f4b865e32e9f3e5b
ExToasty <ExToasty>
2021-01-01 21:25:35 -0700

    Added `unban` command.

---
+++ commands/unban.js
@@ -0,0 +1,37 @@
+const { prefix, embedColor } = require('../config.json');
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'unban',
+	description: 'Unbans the user specified.',
+	usage: `${prefix}unban <id> [reason]`,
+	aliases: ['pardon'],
+	guildOnly: false,
+	args: true,
+	selfExecute: false,
+	requireMention: true,
+	wip: false,
+	cooldown: 5,
+	permissions: ['ADMIN'],
+	category: 'moderation',
+	execute(message, args) {
+		const id = args[0];
+		const reason = args.slice(1).join(' ');
+		const embed = new Discord.MessageEmbed()
+			.setColor(embedColor)
+			.setTitle('__Succesfully Unbanned User__');
+
+		if (!reason) {
+			embed.setDescription(`__**\`${id}\`**__\` has been unbanned\``);
+
+			return message.guild.members.unban(id)
+				.then(message.channel.send(embed))
+				.catch();
+		}
+		embed.setDescription(`__**\`${id}\`**__\` has been unbanned for ${reason}\``);
+
+		return message.guild.members.unban(id, [reason])
+			.then(message.channel.send(embed))
+			.catch();
+	},
+};
