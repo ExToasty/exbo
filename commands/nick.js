0da24ae060852c05f63620a685875734440f8a94
ExToasty <ExToasty>
2021-01-02 14:29:49 -0700

    Added `>nick` command.

---
+++ commands/nick.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const { prefix, embedColor } = require('../config.json');
+
+module.exports = {
+	name: 'nick',
+	description: 'Sets the nickname of the user specified',
+	usage: `${prefix}nick <user/nickname> [nickname]`,
+	aliases: ['nickname', 'setnick', 'setnickname'],
+	guildOnly: true,
+	args: true,
+	selfExecute: true,
+	requireMention: false,
+	wip: false,
+	cooldown: 2,
+	deleteMessage: true,
+	permissions: ['MANAGE_NICKNAMES'],
+	category: 'moderation',
+	execute(message, args) {
+		const member = message.mentions.members.first();
+		const nick = args.slice(1).join(' ');
+		const embed = new Discord.MessageEmbed()
+			.setColor(embedColor)
+			.setTitle('__Succesfully Changed Nickname__');
+
+		if (!member) {
+			embed
+				.setDescription(`\`${message.author.user.tag} changed their nickname to "${nick}"\``);
+			return member.setNickname(nick).then(message.channel.send(embed));
+		}
+		embed.setDescription(`\`${message.author.user.tag} changed ${member.user.tag}'s nickname to ${args[1]}.\``);
+		return member.setNickname(nick).then(message.channel.send(embed));
+	},
+};
