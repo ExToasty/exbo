e96a08f51fa135ae86b8a80f67984628d78c52ed
ExToasty <ExToasty>
2021-01-04 12:01:15 -0700

    fixing unban command.

--- commands/unban.js
+++ commands/unban.js
@@ -15,16 +15,14 @@ module.exports = {
 	cooldown: 5,
 	permissions: ['ADMIN'],
 	category: 'moderation',
-	async execute(message, args, user) {
-		const banList = await message.guild.fetchBans();
-		const id = args[0];
-		const bannedUser = banList.find(findUser => findUser.id === id);
+	async execute(message, args) {
+		const user = message.users.fetch(args[0]);
 		const reason = args.slice(1).join(' ');
 		const embed = new Discord.MessageEmbed()
 			.setColor(embedColor)
 			.setTitle('__Succesfully Unbanned User__');
 		try {
-			if (!bannedUser) {
+			if (!user) {
 				embed
 					.setTitle('__Ban Unsuccesful__')
 					.setDescription('`The user provided isn\'t banned or doesn\'t exist.`');
@@ -35,13 +33,13 @@ module.exports = {
 			if (!reason) {
 				embed.setDescription(`__**\`${user}\`**__\` has been unbanned\``);
 
-				return message.guild.members.unban(id)
+				return message.guild.members.unban(user)
 					.then(message.channel.send(embed))
 					.catch();
 			}
 			embed.setDescription(`__**\`${user}\`**__\` has been unbanned for ${reason}\``);
 
-			return message.guild.members.unban(id, [reason])
+			return message.guild.members.unban(user, [reason])
 				.then(message.channel.send(embed))
 				.catch();
 		}
