602c22fb03089333f153db2f3adc6aa16e40a541
ExToasty <ExToasty>
2021-01-04 12:24:30 -0700

    might've fixed it?

--- commands/unban.js
+++ commands/unban.js
@@ -16,37 +16,33 @@ module.exports = {
 	permissions: ['ADMIN'],
 	category: 'moderation',
 	async execute(message, args) {
-		const bans = message.guild.fetchBans();
-		const user = bans.find(findUser => findUser.id === args[0]);
-		const reason = args.slice(1).join(' ');
-		const embed = new Discord.MessageEmbed()
-			.setColor(embedColor)
-			.setTitle('__Succesfully Unbanned User__');
+		try {
+			const bans = await message.guild.fetchBans();
+			const user = bans.find(findUser => findUser.id === `${args[0]}`);
+			const reason = args.slice(1).join(' ');
+			const embed = new Discord.MessageEmbed()
+				.setColor(embedColor)
+				.setTitle('__Succesfully Unbanned User__');
 
-		if (!args[0]) {
-			embed
-				.setTitle('__Ban Unsuccesful__')
-				.setDescription('`Invalid user ID provided`');
+			if (!user) {
+				embed
+					.setTitle('__Ban Unsuccesful__')
+					.setDescription('`The user provided isn\'t banned or doesn\'t exist.`');
+
+				await message.channel.send(embed);
+			}
+
+			if (!reason) {
+				embed.setDescription(`__**\`${user}\`**__\` has been unbanned\``);
+
+				await message.guild.members.unban(user).then(message.channel.send(embed));
+			}
+			embed.setDescription(`__**\`${user}\`**__\` has been unbanned for ${reason}\``);
+
+			await message.guild.members.unban(user, [reason]).then(message.channel.send(embed));
 		}
-		if (!user) {
-			embed
-				.setTitle('__Ban Unsuccesful__')
-				.setDescription('`The user provided isn\'t banned or doesn\'t exist.`');
-
-			return message.channel.send(embed).catch();
+		catch (err) {
+			console.error(err);
 		}
-
-		if (!reason) {
-			embed.setDescription(`__**\`${user}\`**__\` has been unbanned\``);
-
-			return message.guild.members.unban(user)
-				.then(message.channel.send(embed))
-				.catch();
-		}
-		embed.setDescription(`__**\`${user}\`**__\` has been unbanned for ${reason}\``);
-
-		return message.guild.members.unban(user, [reason])
-			.then(message.channel.send(embed))
-			.catch();
 	},
 };
