aa8460ac6f5cc56076814ec83ef55b58f87b69a2
ExToasty <ExToasty>
2021-01-05 18:30:26 -0700

    updated embed message

--- commands/lock.js
+++ commands/lock.js
@@ -19,7 +19,7 @@ module.exports = {
 		const embed = new Discord.MessageEmbed()
 			.setColor(embedColor)
 			.setTitle('__Channel Is Now Locked__')
-			.setDescription('`This channel is now locked, please do not carry the converstation over to another channel, doing so may lead to a warning or a mute.`');
+			.setDescription('`Only moderators and above can talk here now. Please do not carry the converstation over to another channel, doing so may lead to a warning or a mute.`');
 		return message.channel.updateOverwrite(message.channel.guild.roles.everyone, { SEND_MESSAGES: false })
 			.then(message.channel.send(embed))
 			.catch();
