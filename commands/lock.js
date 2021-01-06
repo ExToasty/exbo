facbfeb8ea214f07d52cae9c9f5a26e60845859d
ExToasty <ExToasty>
2021-01-05 18:28:18 -0700

    didnt point to my config file properly.

--- commands/lock.js
+++ commands/lock.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const { prefix, embedColor } = require('../config.js');
+const { prefix, embedColor } = require('../config.json');
 
 module.exports = {
 	name: 'lock',
