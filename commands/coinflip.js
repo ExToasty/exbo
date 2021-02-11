70aebc28dc4882a7205e2457cd9372cc3e7f6f6e
ExToasty <ExToasty>
2021-02-11 12:03:51 -0700

    Added support for custom sides for coinflip.

--- commands/coinflip.js
+++ commands/coinflip.js
@@ -3,7 +3,7 @@ const { prefix } = require('../config.json');
 module.exports = {
 	name: 'coinflip',
 	description: 'Returns "heads" or "tails"',
-	usage: `${prefix}coinflip`,
+	usage: `${prefix}coinflip <custom side 1> <custom side 2>`,
 	aliases: ['flip', 'coin'],
 	guildOnly: false,
 	minArgs: 0,
@@ -13,12 +13,19 @@ module.exports = {
 	deleteMessage: true,
 	permissions: ['SEND_MESSAGES'],
 	category: 'fun',
-	execute(message) {
+	execute(message, args) {
 		message.delete({ timeout: 1000 });
 
 		const responses = ['heads', 'tails'];
 		const response = responses[Math.floor(Math.random() * responses.length)];
 
-		message.channel.send(`The coin landed on ${response}.`).catch(console.error());
+		if (!args.length) {
+			return message.channel.send(`The coin landed on ${response}.`).catch(console.error());
+		}
+		if (args.length !== 2) {
+			return message.channel.send('You must only provide two custom arguements to the coinflip command.');
+		}
+		const customResponse = args[Math.floor(Math.random() * args.length)];
+		message.channel.send(`You landed on ${customResponse}`).catch(console.error());
 	},
 };
