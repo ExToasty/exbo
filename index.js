476a19b7edb75d50bd7eb9e6264a91021e2533c3
ExToasty <ExToasty>
2021-02-12 15:18:30 -0700

    i messed up sequelize and its giving me errors.

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ const cooldowns = new Discord.Collection();
 const sequelize = new Sequelize('database', 'user', 'password', {
 	host: 'localhost',
 	dialect: 'sqlite',
-	logging: true,
+	logging: false,
 	storage: 'database.sqlite',
 });
 
